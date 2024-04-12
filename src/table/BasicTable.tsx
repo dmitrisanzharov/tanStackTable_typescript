5e62e87f51695b68596bd92c91eb09606ce72950
fruitloaf <dmitrisanzharov@gmail.com>
2024-04-12 19:35:05 +0100

    all good

--- src/table/BasicTable.tsx
+++ src/table/BasicTable.tsx
@@ -1,5 +1,11 @@
 import React from "react";
-import { useReactTable } from "@tanstack/react-table";
+import {
+	useReactTable,
+	flexRender,
+	getCoreRowModel,
+} from "@tanstack/react-table";
+import data from "./data";
+import { columnDef } from "./columnDef";
 import {
 	TableContainer,
 	Paper,
@@ -13,11 +19,76 @@ import {
 type Props = {};
 
 const BasicTable = (props: Props) => {
+	const dataMemo = React.useMemo(() => data, [data]);
+	const columnsMemo = React.useMemo(() => columnDef, [columnDef]);
+
+	const table = useReactTable({
+		data: dataMemo,
+		columns: columnsMemo,
+		getCoreRowModel: getCoreRowModel(),
+		anyKey: 'test',
+	} as any);
+
+
+	// temp block
+	let a1 = table;
+	console.log(a1);
+
+
 	return (
 		<TableContainer component={Paper}>
 			<Table sx={{ minWidth: 650 }} aria-label="simple table">
-                <TableHead></TableHead>
-            </Table>
+				<TableHead sx={{backgroundColor: 'lightgray'}}>
+					{table.getHeaderGroups().map((headerGroup: any) => {
+						// console.log("headerGroup", headerGroup);
+						// console.log("============================");
+						return (
+							<TableRow key={headerGroup.id}>
+								{headerGroup.headers.map(
+									(headerColumn: any) => {
+										// console.log(
+										// 	"headerColumn",
+										// 	headerColumn
+										// );
+										return (
+											<TableCell key={headerColumn.id}>
+												{flexRender(
+													headerColumn.column
+														.columnDef.header,
+													headerColumn.getContext()
+												)}
+											</TableCell>
+										);
+									}
+								)}
+							</TableRow>
+						);
+					})}
+				</TableHead>
+				<TableBody>
+					{table.getRowModel().rows.map((row: any) => {
+						// console.log("============================");
+						// console.log("row", row);
+						// console.log("row", row.getValue('email'));
+						return (
+							<TableRow key={row.id}>
+								{row.getVisibleCells().map((cell: any) => {
+									// console.log("cell", cell);
+									// console.log('cell value', cell.getValue(cell.id))
+									return (
+										<TableCell key={cell.id}>
+											{flexRender(
+												cell.column.columnDef.cell,
+												cell.getContext()
+											)}
+										</TableCell>
+									);
+								})}
+							</TableRow>
+						);
+					})}
+				</TableBody>
+			</Table>
 		</TableContainer>
 	);
 };
--- src/table/columnDef.tsx
+++ src/table/columnDef.tsx
@@ -1,13 +1,18 @@
-import { ColumnDef } from "@tanstack/react-table";
-
 export const columnDef = [
     {
         accessorKey: 'id',
+        header: 'id',
     },
     {
         accessorKey: 'first_name',
+        header: 'First Name'
     },
     {
-        accessorKey: 'last_name'
-    }
+        accessorKey: 'last_name',
+        header: 'Last Name'
+    },
+    {
+        accessorKey: 'email',
+        header: 'Email'
+    },
 ]
