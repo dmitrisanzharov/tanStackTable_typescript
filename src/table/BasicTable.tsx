b2460651c02595714efafc6c04a048ad49d4221b
fruitloaf <dmitrisanzharov@gmail.com>
2024-04-15 18:08:14 +0100

    all good

--- src/table/BasicTable.tsx
+++ src/table/BasicTable.tsx
@@ -14,6 +14,7 @@ import {
 	TableRow,
 	TableCell,
 	TableBody,
+	Box,
 } from "@mui/material";
 
 type Props = {};
@@ -26,70 +27,72 @@ const BasicTable = (props: Props) => {
 		data: dataMemo,
 		columns: columnsMemo,
 		getCoreRowModel: getCoreRowModel(),
-		anyKey: 'test',
+		anyKey: "test",
 	} as any);
-
 
 	// temp block
 	let a1 = table;
 	console.log(a1);
 
-
 	return (
-		<TableContainer component={Paper}>
-			<Table sx={{ minWidth: 650 }} aria-label="simple table">
-				<TableHead sx={{backgroundColor: 'lightgray'}}>
-					{table.getHeaderGroups().map((headerGroup: any) => {
-						// console.log("headerGroup", headerGroup);
-						// console.log("============================");
-						return (
-							<TableRow key={headerGroup.id}>
-								{headerGroup.headers.map(
-									(headerColumn: any) => {
-										// console.log(
-										// 	"headerColumn",
-										// 	headerColumn
-										// );
+		<Box>
+			<TableContainer component={Paper}>
+				<Table sx={{ minWidth: 650 }} aria-label="simple table">
+					<TableHead sx={{ backgroundColor: "lightgray" }}>
+						{table.getHeaderGroups().map((headerGroup: any) => {
+							// console.log("headerGroup", headerGroup);
+							// console.log("============================");
+							return (
+								<TableRow key={headerGroup.id}>
+									{headerGroup.headers.map(
+										(headerColumn: any) => {
+											// console.log(
+											// 	"headerColumn",
+											// 	headerColumn
+											// );
+											return (
+												<TableCell
+													key={headerColumn.id}
+												>
+													{flexRender(
+														headerColumn.column
+															.columnDef.header,
+														headerColumn.getContext()
+													)}
+												</TableCell>
+											);
+										}
+									)}
+								</TableRow>
+							);
+						})}
+					</TableHead>
+					<TableBody>
+						{table.getRowModel().rows.map((row: any) => {
+							// console.log("============================");
+							// console.log("row", row);
+							// console.log("row", row.getValue('email'));
+							return (
+								<TableRow key={row.id}>
+									{row.getVisibleCells().map((cell: any) => {
+										// console.log("cell", cell);
+										// console.log('cell value', cell.getValue(cell.id))
 										return (
-											<TableCell key={headerColumn.id}>
+											<TableCell key={cell.id}>
 												{flexRender(
-													headerColumn.column
-														.columnDef.header,
-													headerColumn.getContext()
+													cell.column.columnDef.cell,
+													cell.getContext()
 												)}
 											</TableCell>
 										);
-									}
-								)}
-							</TableRow>
-						);
-					})}
-				</TableHead>
-				<TableBody>
-					{table.getRowModel().rows.map((row: any) => {
-						// console.log("============================");
-						// console.log("row", row);
-						// console.log("row", row.getValue('email'));
-						return (
-							<TableRow key={row.id}>
-								{row.getVisibleCells().map((cell: any) => {
-									// console.log("cell", cell);
-									// console.log('cell value', cell.getValue(cell.id))
-									return (
-										<TableCell key={cell.id}>
-											{flexRender(
-												cell.column.columnDef.cell,
-												cell.getContext()
-											)}
-										</TableCell>
-									);
-								})}
-							</TableRow>
-						);
-					})}
-				</TableBody>
-			</Table>
-		</TableContainer>
+									})}
+								</TableRow>
+							);
+						})}
+					</TableBody>
+				</Table>
+			</TableContainer>
+		</Box>
 	);
 };
 
