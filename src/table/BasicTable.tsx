8bd5bc92ffd4f04bf457c08db0338a4edf88898e
fruitloaf <dmitrisanzharov@gmail.com>
2024-06-21 14:34:47 +0100

    all good

--- src/table/BasicTable.tsx
+++ src/table/BasicTable.tsx
@@ -1,100 +1,17 @@
-import React from "react";
-import {
-	useReactTable,
-	flexRender,
-	getCoreRowModel,
-} from "@tanstack/react-table";
-import data from "./data";
-import { columnDef } from "./columnDef";
-import {
-	TableContainer,
-	Paper,
-	Table,
-	TableHead,
-	TableRow,
-	TableCell,
-	TableBody,
-	Box,
-} from "@mui/material";
+import React from 'react'
+import allData from './data';
 
-type Props = {};
+type Props = {}
 
 const BasicTable = (props: Props) => {
-	const dataMemo = React.useMemo(() => data, [data]);
-	const columnsMemo = React.useMemo(() => columnDef, [columnDef]);
 
-	const table = useReactTable({
-		data: dataMemo,
-		columns: columnsMemo,
-		getCoreRowModel: getCoreRowModel(),
-		anyKey: "test",
-	} as any);
+    const dataWithMemo = React.useMemo(()=> allData, [allData])
+    console.log("dataWithMemo: ", dataWithMemo);
 
-	// temp block
-	let a1 = table;
-	console.log(a1);
 
-	return (
-		<Box>
-			<TableContainer component={Paper}>
-				<Table sx={{ minWidth: 650 }} aria-label="simple table">
-					<TableHead sx={{ backgroundColor: "lightgray" }}>
-						{table.getHeaderGroups().map((headerGroup: any) => {
-							// console.log("headerGroup", headerGroup);
-							// console.log("============================");
-							return (
-								<TableRow key={headerGroup.id}>
-									{headerGroup.headers.map(
-										(headerColumn: any) => {
-											// console.log(
-											// 	"headerColumn",
-											// 	headerColumn
-											// );
-											return (
-												<TableCell
-													key={headerColumn.id}
-													sx={{fontWeight: 'bold'}}
-												>
-													{flexRender(
-														headerColumn.column
-															.columnDef.header,
-														headerColumn.getContext()
-													)}
-												</TableCell>
-											);
-										}
-									)}
-								</TableRow>
-							);
-						})}
-					</TableHead>
-					<TableBody>
-						{table.getRowModel().rows.map((row: any) => {
-							// console.log("============================");
-							// console.log("row", row);
-							// console.log("row", row.getValue('email'));
-							return (
-								<TableRow key={row.id}>
-									{row.getVisibleCells().map((cell: any) => {
-										// console.log("cell", cell);
-										// console.log('cell value', cell.getValue(cell.id))
-										return (
-											<TableCell key={cell.id}>
-												{flexRender(
-													cell.column.columnDef.cell,
-													cell.getContext()
-												)}
-											</TableCell>
-										);
-									})}
-								</TableRow>
-							);
-						})}
-					</TableBody>
-				</Table>
-			</TableContainer>
-		</Box>
-	);
-};
+  return (
+    <div>BasicTable</div>
+  )
+}
 
-export default BasicTable;
+export default BasicTable
