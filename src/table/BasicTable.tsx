f7e3f715f6d2b795cabca31e2e91f0d0f99905d8
fruitloaf <dmitrisanzharov@gmail.com>
2024-08-28 20:12:51 +0100

    gg

--- src/table/BasicTable.tsx
+++ src/table/BasicTable.tsx
@@ -1,11 +1,50 @@
-import React from 'react'
+import React from "react";
+import { useReactTable, getCoreRowModel, flexRender } from "@tanstack/react-table";
+import { TableContainer, Paper, Table, TableHead, TableRow, TableCell, TableBody } from "@mui/material";
+import dataMain from "./data";
+import { colDef } from "./colDef";
 
-type Props = {}
+type Props = {};
 
 const BasicTable = (props: Props) => {
-  return (
-    <div>BasicTable</div>
-  )
-}
+	const colDefMemo: any = React.useMemo(() => colDef, []);
+	const dataMemo: any = React.useMemo(() => dataMain, []);
 
-export default BasicTable
+	const table = useReactTable({
+		columns: colDefMemo,
+		data: dataMemo,
+		getCoreRowModel: getCoreRowModel(),
+	});
+
+	return (
+		<TableContainer>
+			<Table>
+				<TableHead sx={{ backgroundColor: "lightgray", boxShadow: "inset 0px 0px 0px 1px black"}}>
+					{table.getHeaderGroups().map((headerGroup: any) => {
+						// console.log("headerGroup", headerGroup);
+						return (
+							<TableRow key={headerGroup.id}>
+								{headerGroup.headers.map((headerCell: any) => {
+									// console.log("headerCell", headerCell);
+									return <TableCell key={headerCell.id}>{flexRender(headerCell.column.columnDef.header, headerCell.getContext())}</TableCell>;
+								})}
+							</TableRow>
+						);
+					})}
+				</TableHead>
+				{table.getRowModel().rows.map((row: any) => {
+					// console.log("row", row);
+					return (
+						<TableRow key={row.id}>
+							{row.getVisibleCells().map((cell: any) => {
+								return <TableCell key={cell.id}>{flexRender(cell.column.columnDef.cell, cell.getContext())}</TableCell>;
+							})}
+						</TableRow>
+					);
+				})}
+			</Table>
+		</TableContainer>
+	);
+};
+
+export default BasicTable;
---
+++ src/table/colDef.tsx
@@ -0,0 +1,15 @@
+
+export const colDef: any = [
+    {
+        accessorKey: 'id',
+        header: 'ID'
+    },
+    {
+        accessorKey: 'first_name',
+        header: 'Name'
+    },
+    {
+        accessorKey: 'last_name',
+        header: 'Surname'
+    }
+]
